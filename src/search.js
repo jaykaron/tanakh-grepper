9fd74fc4e876cd5ab3e256b2a64a526bb61fd95e
jaykaron <jaykaron@gmail.com>
2020-04-28 15:24:22 -0400

    Switch to JSON searching

--- src/search.js
+++ src/search.js
@@ -1,20 +1,41 @@
 
-const loadBook = (section, book) => new Promise((resolve, reject) => {
-  fetch(require(`./texts/${section}/${book}.txt`))
-    .then(resp => resolve(resp.text()))
-    .catch(reject)
-})
+const loadJsonBook = (section, book, chapter) => {
+  const bookData = require(`./jsonText/${section}/${book}.json`)
+  const text = bookData['text']
+  if (Number.isInteger(chapter)) {
+    return [text[chapter]]
+  }
+  return text
+}
 
 
 const searchBook = async (section, book, regexStr) => {
-  let text = await loadBook(section, book)
-  let rgx = new RegExp(regexStr, 'g')
-  let matches = text.match(rgx)
+  let text = await loadJsonBook(section, book)
+  let regex = new RegExp(regexStr, 'g')
+
+  let matches = []
+  text.forEach((chapter, i) => {
+    let chapterMatches = searchChapter(chapter, regex)
+    chapterMatches.forEach(match => {
+      matches.push({ book, ...match, chapter: i + 1 })
+    })
+  })
+  return matches
+}
+
+const searchChapter = (chapter, regex) => {
+  let matches = []
+  chapter.forEach((line, i) => {
+    let match = regex.exec(line)
+    if (match) {
+      matches.push({ line: i + 1, match: match[0], text: match.input })
+    }
+  })
   return matches
 }
 
 const TOC = {
-  'Torah': ['Deuteronomy', 'Exodus', 'Genesis', 'Leviticus', 'Numbers'],
+  'Torah': ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy'],
   'Prophets': ['Amos', 'Jeremiah', 'Micah', 'Chronicles1', 'Joel', 'Nahum',
     'Chronicles2', 'Jonah', 'Obadiah', 'Ezekiel', 'Joshua', 'Samuel1',
     'Habakkuk', 'Judges', 'Samuel2', 'Haggai', 'Kings1', 'Zechariah',
@@ -26,6 +47,4 @@ const TOC = {
 
 
 
-export { searchBook, TOC }
-
-
+export { searchBook, TOC, loadJsonBook }
