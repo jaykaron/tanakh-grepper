89fd75789108395cd5893369dd5a81a673816efc
jaykaron <jaykaron@gmail.com>
2020-04-29 12:36:44 -0400

    Redo searching and rename

---
+++ src/textContent.js
@@ -0,0 +1,93 @@
+
+const loadJsonBook = (section, book, chapter) => {
+  const bookData = require(`./jsonText/${section}/${book}.json`)
+  const text = bookData['text']
+  if (Number.isInteger(chapter)) {
+    return text[chapter - 1]
+  }
+  return text
+}
+
+const search = async (section, book, chapter, regexStr) => {
+  const allOrEmpty = str => str === 'All' || str === ''
+  let regex = new RegExp(regexStr, 'g')
+  if (allOrEmpty(section)) {
+    return searchAll(regex)
+  } else if (allOrEmpty(book)) {
+    return searchSection(section, regex)
+  } else if (allOrEmpty(chapter)) {
+    return searchBook(section, book, regex)
+  }
+  return searchChapter(section, book, chapter, regex)
+}
+
+const searchAll = regex => {
+  let resultPromises = sectionTitles().map(section => searchSection(section, regex))
+  return Promise.all(resultPromises)
+    .then(values => values.flat())
+}
+
+const searchSection = (section, regex) => {
+  let resultPromises = bookTitles(section).map(book => searchBook(section, book, regex))
+  return Promise.all(resultPromises)
+    .then(values => values.flat())
+}
+
+const searchBook = async (section, book, regex) => {
+  let text = await loadJsonBook(section, book)
+
+  let matches = []
+  text.forEach((chapter, i) => {
+    let chapterMatches = _searchChapter(chapter, regex)
+      .map(match => ({ ...match, section, book, chapter: i + 1 }))
+    matches.push(...chapterMatches)
+  })
+  return matches
+}
+
+const searchChapter = async (section, book, chapter, regex) => {
+  let text = await loadJsonBook(section, book, chapter)
+  let matches = _searchChapter(text, regex)
+  return matches.map(match => ({ ...match, section, book, chapter }))
+}
+
+const _searchChapter = (chapterText, regex) => {
+  let matches = []
+  chapterText.forEach((line, j) => {
+    let match = searchLine(line, regex)
+    if (match) {
+      matches.push({ ...match, line: j + 1 })
+    }
+  })
+  return matches
+}
+
+const searchLine = (line, regex) => {
+  let match = regex.exec(line)
+  if (match) {
+    return { match: match[0], text: match.input }
+  }
+}
+
+const _TOC = {
+  'Torah': ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy'],
+  'Prophets': ['Amos', 'Jeremiah', 'Micah', 'Joel', 'Nahum',
+    'Jonah', 'Obadiah', 'Ezekiel', 'Joshua', 'Samuel1',
+    'Habakkuk', 'Judges', 'Samuel2', 'Haggai', 'Kings1', 'Zechariah',
+    'Hosea', 'Kings2', 'Zephaniah', 'Isaiah', 'Malachi'],
+  'Writings': ['Daniel', 'Job', 'Psalms', 'Ecclesiastes', 'Lamentations', 'Ruth',
+    'Chronicles1', 'Chronicles2',
+    'Esther', 'Nehemiah', 'SongOfSongs', 'Ezra', 'Proverbs']
+}
+
+const sectionTitles = () => Object.keys(_TOC)
+const bookTitles = section => _TOC[section] || []
+const numChapters = async (section, book) => {
+  if (sectionTitles().includes(section) && bookTitles(section).includes(book)) {
+    let bookText = await loadJsonBook(section, book)
+    return bookText.length
+  }
+  return 0
+}
+
+export { search, sectionTitles, bookTitles, numChapters }
