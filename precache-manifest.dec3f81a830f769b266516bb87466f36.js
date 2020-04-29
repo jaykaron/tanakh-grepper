3dbc53111e480569fdba0dad63febad9b2e85a8f
jaykaron <jaykaron@gmail.com>
2020-04-29 13:34:04 -0400

    Updates

---
+++ precache-manifest.dec3f81a830f769b266516bb87466f36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183cf755fbf6634458ff7a43b19b268c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8302014da79a0c1e3b80",
+    "url": "/static/css/main.a702b924.chunk.css"
+  },
+  {
+    "revision": "0f8d0f98485672e11c5b",
+    "url": "/static/js/2.95509eea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.95509eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8302014da79a0c1e3b80",
+    "url": "/static/js/main.867e5865.chunk.js"
+  },
+  {
+    "revision": "2d30a7a973eae3d6bc4a",
+    "url": "/static/js/runtime-main.fbb335ea.js"
+  }
+]);
