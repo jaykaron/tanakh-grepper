9e7a57f1c9fc6bade85fcc6d9a995c1175ff9f28
jaykaron <jaykaron@gmail.com>
2020-05-12 14:59:02 -0400

    Github corner and more welcome text

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Button, IconButton, SvgIcon, makeStyles } from '@material-ui/core'
 import Search from './components/Search'
 import PassukLister from './components/PassukLister'
 import Welcome from './components/Welcome'
+import GithubCorner from './components/GithubCorner'
 
 const useStyles = makeStyles(() => ({
   main: {
@@ -32,27 +33,30 @@ function App() {
   const classes = useStyles()
 
   return (
-    <div className={classes.main}>
-      <Welcome open={welcomeOpen} onClose={() => setWelcomeOpen(false)} />
-      <Search setResults={setResults} lang={lang} />
-      <PassukLister lang={lang} passukim={results} />
-      <div className={classes.bottomButtons}>
-        <IconButton onClick={() => setWelcomeOpen(true)}>
-          <SvgIcon>
-            <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-          </SvgIcon>
-        </IconButton>
-        <Button variant='contained' size='small'
-          onClick={() => {
-            setLang(lang === 'en' ? 'he' : 'en')
-          }}>
-          {lang === 'en' ? 'עברית' : 'English'}
-        </Button>
+    <div>
+      <GithubCorner />
+      <div className={classes.main}>
+        <Welcome open={welcomeOpen} onClose={() => setWelcomeOpen(false)} />
+        <Search setResults={setResults} lang={lang} />
+        <PassukLister lang={lang} passukim={results} />
+        <div className={classes.bottomButtons}>
+          <IconButton onClick={() => setWelcomeOpen(true)}>
+            <SvgIcon>
+              <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </SvgIcon>
+          </IconButton>
+          <Button variant='contained' size='small'
+            onClick={() => {
+              setLang(lang === 'en' ? 'he' : 'en')
+            }}>
+            {lang === 'en' ? 'עברית' : 'English'}
+          </Button>
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,6 +7,10 @@ import {
 
 const WIKI_URL = 'https://en.wikipedia.org/wiki/Regular_expression'
 const BLOG_URL = 'https://codular.com/regex'
+const PAL_URL = 'https://www.regexpal.com/'
+const SEFARIA_URL = 'http://sefaria.org/'
+const REGEX_DOCS_URL = 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp'
+const ISSUES_URL = 'https://github.com/jaykaron/tanakh-grepper/issues'
 
 const TRIPLE_REGEX = /(\w)\1\1/.toString().slice(1, -1)
 const ALLIT_REGEX = /(^| )(\w)\w+( \2\w+){4,}/.toString().slice(1, -1)
@@ -37,7 +41,23 @@ const Welcome = ({ open, onClose }) => {
         <DialogText>
           If you aren't familiar with regular expressions they are a powerful way
           of finding patterns in text. You can read up about them
-            on <a href={WIKI_URL}>Wikipedia</a> or this <a href={BLOG_URL}>random blog post</a>.
+          on <a href={WIKI_URL}>Wikipedia</a> or this <a href={BLOG_URL}>random blog post</a>.
+          I often use <a href={PAL_URL}>RegexPal</a> to help build complicated expressions.
+        </DialogText>
+        <DialogText variant='h5'>Technical Notes:</DialogText>
+        <DialogText>
+          <ul>
+            <li>Text comes from <a href={SEFARIA_URL}>Sefaria.org</a></li>
+            <li>To facillitate searching in Hebrew <code>\w</code> gets replaced with <code>[א-ת]</code></li>
+            <li>Searches are automatically global</li>
+            <li>
+              Searching is done in JavaScript.
+              Read the <a href={REGEX_DOCS_URL}>docs</a> for the fine details
+            </li>
+            <li>
+              You can leave comments or issues on <a href={ISSUES_URL}>GitHub</a>
+            </li>
+          </ul>
         </DialogText>
       </DialogContent>
     </Dialog >
