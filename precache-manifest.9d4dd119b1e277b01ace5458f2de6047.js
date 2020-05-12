4b896933b0522b3aa1655e9768179fed414da4ce
jaykaron <jaykaron@gmail.com>
2020-05-12 15:00:13 -0400

    Updates

---
+++ precache-manifest.9d4dd119b1e277b01ace5458f2de6047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebe31e73812789ce94068ecc639a4f0",
+    "url": "/tanakh-grepper/index.html"
+  },
+  {
+    "revision": "d3cbb5b5fad8d8e3e8e8",
+    "url": "/tanakh-grepper/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0cf1395fc9322046ddef",
+    "url": "/tanakh-grepper/static/js/2.e648bab3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tanakh-grepper/static/js/2.e648bab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3cbb5b5fad8d8e3e8e8",
+    "url": "/tanakh-grepper/static/js/main.716fed40.chunk.js"
+  },
+  {
+    "revision": "ac95ebba1097f78bbb6b",
+    "url": "/tanakh-grepper/static/js/runtime-main.e9b2a3bc.js"
+  }
+]);
