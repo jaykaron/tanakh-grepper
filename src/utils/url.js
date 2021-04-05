670ea1b06578d5d2f1661cae2f8da659c7a0d495
jaykaron <jaykaron@gmail.com>
2021-04-05 18:39:47 -0400

    Nest url parsing and parseInt for chapter

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,5 @@
+import { bookTitles, sectionTitles } from "../textContent";
+
 export function stateFromUrl() {
   if (window.location.search) {
     const searchParams = new URLSearchParams(window.location.search);
@@ -7,18 +9,20 @@ export function stateFromUrl() {
       search.regex = regex;
     }
     const section = searchParams.get("section");
-    if (section) {
+    if (section && sectionTitles().includes(section)) {
       search.section = section;
       search.book = "";
       search.chapter = "";
-    }
-    const book = searchParams.get("book");
-    if (book) {
-      search.book = book;
-    }
-    const chapter = searchParams.get("chapter");
-    if (chapter) {
-      search.chapter = chapter;
+
+      const book = searchParams.get("book");
+      if (book && bookTitles(section).includes(book)) {
+        search.book = book;
+
+        const chapter = searchParams.get("chapter");
+        if (chapter && parseInt(chapter)) {
+          search.chapter = parseInt(chapter);
+        }
+      }
     }
     return search;
   }
@@ -26,16 +30,16 @@ export function stateFromUrl() {
 }
 
 export function setQueryUrl(search) {
-  const location = window.location;
+  const loc = window.location;
 
-  const searchParams = new URLSearchParams(location.search);
+  const searchParams = new URLSearchParams(loc.search);
   searchParams.set("regex", search.regex);
   searchParams.set("section", search.section || "");
   searchParams.set("book", search.book || "");
   searchParams.set("chapter", search.chapter || "");
 
-  const newUrl = `${location.protocol}//${location.host}${
-    location.pathname
+  const newUrl = `${loc.protocol}//${loc.host}${
+    loc.pathname
   }?${searchParams.toString()}`;
   window.history.pushState({ path: newUrl }, "", newUrl);
 }
