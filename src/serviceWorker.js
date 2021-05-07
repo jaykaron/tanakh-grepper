500568dfbbfada408ebb82281519b57b9a7e50f2
jaykaron <jaykaron@gmail.com>
2021-05-07 19:08:18 -0400

    Add default service-worker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -56,7 +56,7 @@ export function register(config) {
 
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
-    .register(swUrl, { scope: process.env.PUBLIC_URL })
+    .register(swUrl)
     .then((registration) => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
