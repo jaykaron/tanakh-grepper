f3fd06203ad90a7922827fc79ec7e94d7b80b42b
Jay Karon <jay.karon@mapsted.com>
2022-03-24 09:07:14 -0400

    Fix layering of kofi widget and catch errors

--- src/components/SupportButton.js
+++ src/components/SupportButton.js
@@ -1,22 +1,32 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
+/**
+ * Call the Ko-Fi script to render the widget.
+ * The component doesn't return anything because the script creates the whole widget.
+ * @returns {null}
+ */
 const SupportButton = () => {
   useEffect(() => {
     if (window.kofiWidgetOverlay) {
-      window.kofiWidgetOverlay.draw(
-        "jaykaron",
-        {
-          type: "floating-chat",
-          "floating-chat.donateButton.text": "Support me",
-          "floating-chat.donateButton.background-color": "#303f9f",
-          "floating-chat.donateButton.text-color": "#fff",
-        },
-        "kofi-container"
-      );
+      try {
+        window.kofiWidgetOverlay.draw(
+          "jaykaron",
+          {
+            type: "floating-chat",
+            "floating-chat.donateButton.text": "Support me",
+            "floating-chat.donateButton.background-color": "#303f9f",
+            "floating-chat.donateButton.text-color": "#fff",
+          },
+          "kofi-root"
+        );
+      } catch (error) {
+        console.error("Error rendering Ko-Fi widget:", error);
+      }
     }
+    return () => (document.getElementById("kofi-root").innerHTML = "");
   }, []);
 
-  return <div id="kofi-container" />;
+  return null;
 };
 
 export default SupportButton;
