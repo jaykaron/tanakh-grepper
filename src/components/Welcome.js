ea04048b331fcf01eac887a241a665f6df70baca
Jay Karon <jkaron@oceanmd.com>
2026-05-03 22:59:19 -0400

    Update welcome text

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,7 +8,6 @@ import {
 import { searchToUrl } from "../utils/url";
 
 const WIKI_URL = "https://en.wikipedia.org/wiki/Regular_expression";
-const BLOG_URL = "https://codular.com/regex";
 const PAL_URL = "https://www.regexpal.com/";
 const SEFARIA_URL = "http://sefaria.org/";
 const REGEX_DOCS_URL =
@@ -37,12 +36,17 @@ const Welcome = ({ open, onClose }) => {
     >
       <DialogContent>
         <DialogText variant="h3">
-          Welcome to a new way to search Tanakh!
+          Welcome to Tanakh Grepper!
         </DialogText>
         <DialogText>
-          Welcome to the site made for people who were just not satisfied with
-          the current options to search through Tanakh (and who have working
-          experience with regular expressions).
+          Tanakh Grepper is a Hebrew-text Tanakh search engine. It can be used to search for exact
+          word matches like one would expect. <small>(Ex. search for all occurrences of the word <a href={searchToUrl({ regex: "אדמה" })}>אדמה</a>)</small> 
+        </DialogText>
+        <DialogText>
+          You can also narrow down the search by section, book or chapter. <small>(Ex. Every mention of <a href={searchToUrl({ regex: "בגד", section: "Torah", book: "Genesis" })}>בגד in Genesis</a>)</small>
+        </DialogText>
+        <DialogText>
+          But the real power comes from using regular expressions.
         </DialogText>
         <DialogText>
           Like what if you want to find all the times a letter appears 3 times
@@ -65,10 +69,9 @@ const Welcome = ({ open, onClose }) => {
           </small>
         </DialogText>
         <DialogText>
-          If you aren't familiar with regular expressions, they are a powerful
-          way of finding patterns in text. You can read up about them on{" "}
-          <a href={WIKI_URL}>Wikipedia</a> or this{" "}
-          <a href={BLOG_URL}>random blog post</a>. I often use{" "}
+          If you aren't familiar with regular expressions, they are a powerful and fairly
+          standardized way of specifying text patterns that are often used for complex searches. You can read up about them on{" "}
+          <a href={WIKI_URL}>Wikipedia</a> or many other places on the internet. I often use{" "}
           <a href={PAL_URL}>RegexPal</a> to help build complicated expressions.
         </DialogText>
         <DialogText variant="h5">Technical Notes:</DialogText>
@@ -78,8 +81,7 @@ const Welcome = ({ open, onClose }) => {
               Text comes from <a href={SEFARIA_URL}>Sefaria.org</a>
             </li>
             <li>
-              To facillitate searching in Hebrew <code>\w</code> gets replaced
-              with <code>[א-ת]</code>
+              To facillitate searching in Hebrew <code>\w</code>, <code>\W</code>, <code>\b</code> and <code>\B</code> are opaquely replaced with patterns that work with Hebrew text.
             </li>
             <li>
               Searching is done in JavaScript. Read the{" "}
